c4bd6adfad0c061978b41d85f2e6add06cda935a
Eduardo Rezende <dudumoises2005@gmail.com>
2025-06-23 20:14:06 -0300

    Adding Unit Test to the GuardThrow class
    Adding tests for the GuardThrow class

---
+++ CleanValidation.Core.Tests/Guards/GuardThrowTest.cs
@@ -0,0 +1,32 @@
+﻿using CleanValidation.Core.Exceptions;
+using CleanValidation.Core.Guards;
+
+namespace CleanValidation.Core.Tests.Guards
+{
+    public class GuardThrowTest
+    {
+        [Fact]
+        public void AgainstNull_NullUser_ThrowsCleanValidationException()
+        {
+            User? user = null;
+                        
+            Assert.Throws<CleanValidationException>(() =>
+            {
+                GuardThrow guardThrow = GuardThrow.Create()
+                .AgainstNull(user);
+            });
+        }
+
+        [Fact]
+        public void AgainstNull_ValidUser_ThrowsNothing()
+        {
+            var user = new User("Parker", 77, "Gentleman");
+
+            GuardThrow guardThrow = GuardThrow.Create()
+                .AgainstNull(user)
+                .AgainstNullOrWhiteSpace(user.Name);
+
+            Assert.NotNull(guardThrow);            
+        }
+    }
+}
