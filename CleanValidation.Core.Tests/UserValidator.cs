44658eb57d055e3a080e29518f406f01056ad394
Eduardo Rezende <dudumoises2005@gmail.com>
2025-06-23 20:28:21 -0300

    Updating UserValidator class
    Adding Validate method for best visualization and practice

--- CleanValidation.Core.Tests/UserValidator.cs
+++ CleanValidation.Core.Tests/UserValidator.cs
@@ -8,20 +8,33 @@ namespace CleanValidation.Core.Tests
 
     public class UserValidator : Validator<User>
     {
-        public override async Task<IResult> ValidateAsync(
-            User value, 
-            string cultureName = "en-US", 
-            CancellationToken cancellationToken = default)
+        public override IResult Validate(User value, string cultureName = "en-US")
         {
-            var result = await base.ValidateAsync(value, cultureName, cancellationToken);
+            IResult result = base.Validate(value, cultureName);
 
             if (!result.Success)
                 return result;
 
             return Guard.Create(cultureName)
-                .AgainstNullOrWhiteSpace(value.Name)
-                .AgainstNullOrWhiteSpace(value.Description)
-                .GetResult();
+               .AgainstNullOrWhiteSpace(value.Name)
+               .AgainstNullOrWhiteSpace(value.Description)
+               .GetResult();
+        }
+
+        public override async Task<IResult> ValidateAsync(
+            User value, 
+            string cultureName = "en-US", 
+            CancellationToken cancellationToken = default)
+        {
+            IResult result = await base.ValidateAsync(value, cultureName, cancellationToken);
+
+            if (!result.Success)
+                return result;
+
+            //Simulates a asynchronously operation
+            await Task.Delay(millisecondsDelay: 350, cancellationToken);
+
+            return SuccessResult.Create();
         }
     }    
 }
