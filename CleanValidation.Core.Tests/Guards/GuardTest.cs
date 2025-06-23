78ef19b2c6f42f163da946fef721715a895cac15
Eduardo Rezende <dudumoises2005@gmail.com>
2025-06-23 20:13:45 -0300

    Adding Unit Test to the Guard class
    Adding tests for the Guard class

---
+++ CleanValidation.Core.Tests/Guards/GuardTest.cs
@@ -0,0 +1,31 @@
+﻿using CleanValidation.Core.Guards;
+using CleanValidation.Core.Results;
+
+namespace CleanValidation.Core.Tests.Guards
+{
+    public class GuardTest
+    {
+        [Fact]
+        public void AgainstNull_NullUser_ReturnsInvalidResultType()
+        {
+            User? user = null;
+            Guard guard = Guard.Create();
+
+            IResult result = guard.AgainstNull(user).GetResult();
+        
+            Assert.NotNull(result);
+            Assert.IsType<ErrorResult>(result);
+        }
+
+        [Fact]
+        public void AgainstNull_ValidUser_ReturnsSuccessResultType() 
+        {
+            var user = new User("Parker", 77, "Gentleman");
+
+            IResult result = Guard.Create().AgainstNull(user).GetResult();
+
+            Assert.NotNull(result);
+            Assert.IsType<SuccessResult>(result);
+        }
+    }
+}
