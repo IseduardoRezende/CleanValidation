abdcc8dba11365daa7c88eda9972d41a8264daac
Eduardo Rezende <dudumoises2005@gmail.com>
2025-06-23 20:14:57 -0300

    Adding Unit Test for the Results Types
    Adding tests for the Results types

---
+++ CleanValidation.Core.Tests/Results/ResultTest.cs
@@ -0,0 +1,26 @@
+﻿using CleanValidation.Core.Errors;
+using CleanValidation.Core.Results;
+
+namespace CleanValidation.Core.Tests.Results
+{
+    public class ResultTest
+    {
+        [Fact]
+        public void SuccessResult_ReturnsSuccess()
+        {
+            var result = SuccessResult.Create();
+
+            Assert.True(result.Success);
+        }
+
+        [Fact]
+        public void ErrorResult_ReturnsFalseSuccess()
+        {
+            var errorMessage = "Error Message";
+            var result = ErrorResult.Create(new Error(errorMessage, field: null));
+
+            Assert.False(result.Success);
+            Assert.Equal(errorMessage, result.Errors.First().Message);
+        }
+    }
+}
