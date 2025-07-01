d699e7775ce34ac18a99b56f65eeb35d6ee86c38
Eduardo Rezende <dudumoises2005@gmail.com>
2025-06-30 21:40:04 -0300

    Adding ValidationBehavior enum
    Adding an enum  to determine how guard clauses proceeds when error occour.

---
+++ src/CleanValidation.Core/Enums/ValidationBehavior.cs
@@ -0,0 +1,18 @@
+﻿namespace CleanValidation.Core.Enums
+{
+    /// <summary>
+    /// Determines how guard clauses proceeds when error occour.
+    /// </summary>
+    public enum ValidationBehavior
+    {
+        /// <summary>
+        /// If one guard clause fails, the validation continues to the next clause.
+        /// </summary>
+        Continue,
+        /// <summary>
+        /// If a guard clause fails, the validation process 
+        /// stops. This can be useful for "fail fast" scenarios.
+        /// </summary>
+        Stop
+    }
+}
