484d40857f442ad80323bf536b3f5f03b8469878
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-09 22:57:33 -0300

    Updating Guard_External methods
    Updating and Improving Against methods

--- src/CleanValidation.Core/Guards/Guard_External.cs
+++ src/CleanValidation.Core/Guards/Guard_External.cs
@@ -1,7 +1,8 @@
 ﻿using System.Net;
-using System.Web;
 using System.Text.Json;
+using System.Net.Sockets;
 using CleanValidation.Core.Errors;
+using CleanValidation.Core.Options;
 using CleanValidation.Core.Results;
 using System.Text.RegularExpressions;
 using System.Runtime.CompilerServices;
@@ -10,31 +11,93 @@ namespace CleanValidation.Core.Guards
 {
     public partial class Guard
     {
-        public Guard AgainstInvalidUri(string? uri, UriKind kind = UriKind.Absolute, string? message = null, [CallerArgumentExpression(nameof(uri))] string? paramName = null)
+        public Guard AgainstInvalidUri(
+            string? uri,
+            UriKind kind = UriKind.Absolute,
+            string? message = null,
+            [CallerArgumentExpression(nameof(uri))] string? paramName = null)
         {
-            if (Continue && !Uri.TryCreate(uri, kind, out _))
+            if (!Continue)
+                return this;
+
+            if (!Uri.IsWellFormedUriString(uri, kind) || !Uri.TryCreate(uri, kind, out Uri? outUri))
+            {
+                Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+                return this;
+            }
+
+            if (outUri.Scheme != Uri.UriSchemeHttp && outUri.Scheme != Uri.UriSchemeHttps)
                 Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
 
             return this;
         }
 
-        public Guard AgainstInvalidIpAddress(string? ip, string? message = null, [CallerArgumentExpression(nameof(ip))] string? paramName = null)
+        [GeneratedRegex(@"^((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.|$)){4}$")]
+        protected static partial Regex StrictIpv4Regex();
+
+        public Guard AgainstInvalidIpAddress(
+            string? ipAddress,
+            IpAddressOptions options = IpAddressOptions.None,
+            string? message = null,
+            [CallerArgumentExpression(nameof(ipAddress))] string? paramName = null)
         {
-            if (Continue && (ip is null || !IPAddress.TryParse(ip, out _)))
+            if (Continue && !IsValidIpAddress(ipAddress, options))
                 Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
 
             return this;
         }
 
-        public Guard AgainstHtml(string? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        protected static bool IsValidIpAddress(
+            string? ipAddress,
+            IpAddressOptions options = IpAddressOptions.None)
         {
-            if (Continue && HttpUtility.HtmlEncode(value) is not null)
+            if (!IPAddress.TryParse(ipAddress, out var ip))
+                return false;
+
+            if ((options & IpAddressOptions.DisallowLeadingZerosInIpv4) != 0 &&
+                ip.AddressFamily == AddressFamily.InterNetwork &&
+                !StrictIpv4Regex().IsMatch(ipAddress))
+            {
+                return false;
+            }
+
+            if ((options & IpAddressOptions.DisallowIpv6) != 0 &&
+                ip.AddressFamily == AddressFamily.InterNetworkV6)
+            {
+                return false;
+            }
+
+            if ((options & IpAddressOptions.DisallowLoopback) != 0 &&
+                IPAddress.IsLoopback(ip))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public Guard AgainstHtml(
+            string? value,
+            string? message = null,
+            [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        {
+            if (!Continue)
+                return this;
+
+            var html = WebUtility.HtmlEncode(value);
+
+            if (string.IsNullOrWhiteSpace(html) || !html.Equals(value))
                 Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
 
             return this;
         }
 
-        public Guard AgainstUnmatchRegex(string? value, string? pattern, RegexOptions options = RegexOptions.None, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        public Guard AgainstUnmatchRegex(
+            string? value,
+            string pattern,
+            RegexOptions options = RegexOptions.None,
+            string? message = null,
+            [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
             if (Continue && (value is null || pattern is null || !Regex.IsMatch(value, pattern, options)))
                 Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
@@ -42,7 +105,11 @@ namespace CleanValidation.Core.Guards
             return this;
         }
 
-        public Guard AgainstInvalidJson<T>(string? json, string? message = null, JsonSerializerOptions? options = null, [CallerArgumentExpression(nameof(json))] string? paramName = null)
+        public Guard AgainstInvalidJson<T>(
+            string? json,
+            JsonSerializerOptions? options = null,
+            string? message = null,
+            [CallerArgumentExpression(nameof(json))] string? paramName = null)
         {
             if (Continue && !IsValidJson<T>(json, options))
                 Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
@@ -69,31 +136,61 @@ namespace CleanValidation.Core.Guards
 
     public partial class Guard<T>
     {
-        new public Guard<T> AgainstInvalidUri(string? uri, UriKind kind = UriKind.Absolute, string? message = null, [CallerArgumentExpression(nameof(uri))] string? paramName = null)
+        new public Guard<T> AgainstInvalidUri(
+            string? uri,
+            UriKind kind = UriKind.Absolute,
+            string? message = null,
+            [CallerArgumentExpression(nameof(uri))] string? paramName = null)
         {
-            if (Continue && !Uri.TryCreate(uri, kind, out _))
+            if (!Continue)
+                return this;
+
+            if (!Uri.IsWellFormedUriString(uri, kind) || !Uri.TryCreate(uri, kind, out Uri? outUri))
+            {
+                Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+                return this;
+            }
+
+            if (outUri.Scheme != Uri.UriSchemeHttp && outUri.Scheme != Uri.UriSchemeHttps)
                 Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
 
             return this;
         }
 
-        new public Guard<T> AgainstInvalidIpAddress(string? ip, string? message = null, [CallerArgumentExpression(nameof(ip))] string? paramName = null)
+        new public Guard<T> AgainstInvalidIpAddress(
+            string? ipAddress,
+            IpAddressOptions options = IpAddressOptions.None,
+            string? message = null,
+            [CallerArgumentExpression(nameof(ipAddress))] string? paramName = null)
         {
-            if (Continue && (ip is null || !IPAddress.TryParse(ip, out _)))
+            if (Continue && !IsValidIpAddress(ipAddress, options))
                 Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
 
             return this;
         }
 
-        new public Guard<T> AgainstHtml(string? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        new public Guard<T> AgainstHtml(
+            string? value,
+            string? message = null,
+            [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
-            if (Continue && HttpUtility.HtmlEncode(value) is not null)
+            if (!Continue)
+                return this;
+
+            var html = WebUtility.HtmlEncode(value);
+
+            if (string.IsNullOrWhiteSpace(html) || !html.Equals(value))
                 Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
 
             return this;
         }
 
-        new public Guard<T> AgainstUnmatchRegex(string? value, string? pattern, RegexOptions options = RegexOptions.None, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        new public Guard<T> AgainstUnmatchRegex(
+            string? value,
+            string pattern,
+            RegexOptions options = RegexOptions.None,
+            string? message = null,
+            [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
             if (Continue && (value is null || pattern is null || !Regex.IsMatch(value, pattern, options)))
                 Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
@@ -101,7 +198,11 @@ namespace CleanValidation.Core.Guards
             return this;
         }
 
-        new public Guard<T> AgainstInvalidJson<TValue>(string? json, string? message = null, JsonSerializerOptions? options = null, [CallerArgumentExpression(nameof(json))] string? paramName = null)
+        new public Guard<T> AgainstInvalidJson<TValue>(
+            string? json,
+            JsonSerializerOptions? options = null,
+            string? message = null,
+            [CallerArgumentExpression(nameof(json))] string? paramName = null)
         {
             if (Continue && !IsValidJson<TValue>(json, options))
                 Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
---
+++ src/CleanValidation.Core/Options/IpAddressOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanValidation.Core.Options
+{
+    [Flags]
+    public enum IpAddressOptions
+    {
+        None = 0,
+        DisallowLeadingZerosInIpv4 = 1,
+        DisallowIpv6 = 2,
+        DisallowLoopback = 4
+    }
+}
