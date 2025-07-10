d2f3ae853a9d9acad73c75540050a61684ad3690
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-10 19:49:09 -0300

    Updating Guard_Numeric methods
    Removing AgainstZero or Negative methods

--- src/CleanValidation.Core/Guards/Guard_Numeric.cs
+++ src/CleanValidation.Core/Guards/Guard_Numeric.cs
@@ -23,43 +23,7 @@ namespace CleanValidation.Core.Guards
                 Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
 
             return this;
-        }
-
-        public Guard AgainstNegative<T>(T? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
-            where T : IComparisonOperators<T, int, bool>
-        {
-            if (Continue && (value is null || value < 0))
-                Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
-
-            return this;
-        }
-
-        public Guard AgainstNegativeOrZero<T>(T? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
-           where T : IComparisonOperators<T, int, bool>
-        {
-            if (Continue && (value is null || value <= 0))
-                Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
-
-            return this;
-        }
-
-        public Guard AgainstZero<T>(T? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
-            where T : INumber<T>
-        {
-            if (Continue && value is 0)
-                Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
-
-            return this;
-        }
-
-        public Guard AgainstNotMultipleOf<T>(T? value, T divisor, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
-                where T : IModulusOperators<T, T, T>, IEqualityOperators<T, int, bool>
-        {
-            if (Continue && (value is null || value % divisor != 0))
-                Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
-
-            return this;
-        }
+        }                      
     }
 
     public partial class Guard<T>
@@ -80,42 +44,6 @@ namespace CleanValidation.Core.Guards
                 Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
 
             return this;
-        }
-
-        new public Guard<T> AgainstNegative<TValue>(TValue? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
-            where TValue : IComparisonOperators<TValue, int, bool>
-        {
-            if (Continue && (value is null || value < 0))
-                Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
-
-            return this;
-        }
-
-        new public Guard<T> AgainstNegativeOrZero<TValue>(TValue? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
-           where TValue : IComparisonOperators<TValue, int, bool>
-        {
-            if (Continue && (value is null || value <= 0))
-                Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
-
-            return this;
-        }
-
-        new public Guard<T> AgainstZero<TValue>(TValue? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
-            where TValue : INumber<TValue>
-        {
-            if (Continue && value is 0)
-                Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
-
-            return this;
-        }
-
-        new public Guard<T> AgainstNotMultipleOf<TValue>(TValue? value, TValue divisor, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
-                where TValue : IModulusOperators<TValue, TValue, TValue>, IEqualityOperators<TValue, int, bool>
-        {
-            if (Continue && (value is null || value % divisor != 0))
-                Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
-
-            return this;
-        }
+        }                      
     }
 }
