8b96ac0ef9fe256ef128c196b1fe578dad98a6d4
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-11 13:15:03 -0300

    Adding new methods on Guard_Type
    Adding AgainstTypeOf, AgainstAssignableFrom and AgainstAssignableTo methods

--- src/CleanValidation.Core/Guards/Guard_Type.cs
+++ src/CleanValidation.Core/Guards/Guard_Type.cs
@@ -30,6 +30,30 @@ namespace CleanValidation.Core.Guards
             return this;
         }
 
+        public Guard AgainstAssignableTo<TBase>(object? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        {
+            if (Continue && (value is null || value.GetType().IsAssignableTo(typeof(TBase))))
+                Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+
+            return this;
+        }
+
+        public Guard AgainstAssignableFrom<TBase>(object? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        {
+            if (Continue && (value is null || value.GetType().IsAssignableFrom(typeof(TBase))))
+                Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+
+            return this;
+        }
+
+        public Guard AgainstTypeOf<TExpected>(object? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        {
+            if (Continue && (value is null || value.GetType().Equals(typeof(TExpected))))
+                Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+
+            return this;
+        }
+
         public Guard AgainstInvalidEnum<TEnum>(object? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
             where TEnum : Enum
         {
@@ -61,6 +85,30 @@ namespace CleanValidation.Core.Guards
         new public Guard<T> AgainstNotTypeOf<TExpected>(object? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
             if (Continue && (value is null || !value.GetType().Equals(typeof(TExpected))))
+                Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+
+            return this;
+        }
+
+        new public Guard<T> AgainstAssignableTo<TBase>(object? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        {
+            if (Continue && (value is null || value.GetType().IsAssignableTo(typeof(TBase))))
+                Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+
+            return this;
+        }
+
+        new public Guard<T> AgainstAssignableFrom<TBase>(object? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        {
+            if (Continue && (value is null || value.GetType().IsAssignableFrom(typeof(TBase))))
+                Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+
+            return this;
+        }
+
+        new public Guard<T> AgainstTypeOf<TExpected>(object? value, string? message = null, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        {
+            if (Continue && (value is null || value.GetType().Equals(typeof(TExpected))))
                 Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
 
             return this;
