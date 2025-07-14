2f2f23b003feba1b3a6d7c7a74dc5a93816e161e
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-13 23:43:27 -0300

    Updating UserValidator
    Updating Validate method

--- tests/CleanValidation.Core.Tests/UserValidator.cs
+++ tests/CleanValidation.Core.Tests/UserValidator.cs
@@ -4,37 +4,24 @@ using CleanValidation.Core.Validators;
 
 namespace CleanValidation.Core.Tests
 {
-    public record User(string Name, byte Age, string Description);
+    public record Base(long Id);
+
+    public record User(long Id, string Name, byte Age) : Base(Id);
+
+    public record Post(long Id, string Tittle, long UserId, string Content, PostType Type) : Base(Id);
+
+    public enum PostType { Games, Programming, Life, Movies }
 
     public class UserValidator : Validator<User>
     {
-        public override IResult Validate(User value, string cultureName = "en-US")
-        {
-            IResult result = base.Validate(value, cultureName);
-
-            if (!result.Success)
-                return result;
-
-            return Guard.Create(cultureName)
-               .AgainstWhiteSpace(value.Name)
-               .AgainstWhiteSpace(value.Description)
-               .GetResult();
-        }
-
-        public override async Task<IResult> ValidateAsync(
-            User value, 
-            string cultureName = "en-US", 
-            CancellationToken cancellationToken = default)
-        {
-            IResult result = await base.ValidateAsync(value, cultureName, cancellationToken);
-
-            if (!result.Success)
-                return result;
-
-            //Simulates a asynchronously operation
-            await Task.Delay(millisecondsDelay: 350, cancellationToken);
-
-            return SuccessResult.Create();
-        }
-    }    
+        public override IResult Validate(User? value, string cultureName = "en-US") =>
+            Guard.Create(cultureName)
+            .AgainstNull(value)
+            .AgainstWhiteSpace(value.Name)
+            .AgainstLessThan(value.Id, min: 1)
+            .AgainstMinLength(value.Name, minLength: 3)
+            .AgainstMaxLength(value.Name, maxLength: 50)
+            .AgainstOutOfRange(value.Age, min: 18, max: 110)
+            .GetResult();
+    }
 }
