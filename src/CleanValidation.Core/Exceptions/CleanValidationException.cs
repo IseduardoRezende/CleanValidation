ef33d38645c5140d87483212c6057373397571db
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-15 16:59:10 -0300

    ♻️refactor: CleanValidationException
    Adding constructor overload for inner exception

--- src/CleanValidation.Core/Exceptions/CleanValidationException.cs
+++ src/CleanValidation.Core/Exceptions/CleanValidationException.cs
@@ -4,11 +4,19 @@
     /// Represents clean validation errors that occur during application execution. 
     /// </summary>
     public class CleanValidationException : Exception
-    {
+    {        
         /// <summary>
         /// Initializes a new instance of the <see cref="CleanValidationException"/> class.
         /// </summary>
         /// <param name="message">Descriptive message.</param>
-        public CleanValidationException(string message) : base(message) { }        
+        public CleanValidationException(string message) : base(message) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CleanValidationException"/> class.
+        /// </summary>
+        /// <param name="message">Descriptive message.</param>
+        /// <param name="innerException">The inner exception.</param>
+        public CleanValidationException(string message, Exception? innerException) 
+            : base(message, innerException) { }
     }
 }
