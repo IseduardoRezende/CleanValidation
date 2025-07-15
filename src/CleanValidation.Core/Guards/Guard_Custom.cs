f409aafa078ed008de56df838dcbd47b452f0cdb
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-15 17:16:16 -0300

    ♻️refactor: Guard_Custom
    Updating Invalid results error message creation

--- src/CleanValidation.Core/Guards/Guard_Custom.cs
+++ src/CleanValidation.Core/Guards/Guard_Custom.cs
@@ -7,71 +7,77 @@ namespace CleanValidation.Core.Guards
     public partial class Guard
     {
         public Guard AgainstTrue(
-            bool? condition, 
-            string? message = null, 
+            bool? condition,
+            string? message = null,
             [CallerArgumentExpression(nameof(condition))] string? paramName = null)
         {
             if (Continue && condition is true or null)
-                Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+                Result = InvalidResult.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstTrue), paramName, CultureName), message));
 
             return this;
         }
 
         public Guard AgainstTrue<T>(
-            T? value, 
-            Func<T, bool> func, 
-            string? message = null, 
+            T? value,
+            Func<T, bool> func,
+            string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
             if (Continue && (value is null || func.Invoke(value)))
-                Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+                Result = InvalidResult.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstTrue), paramName, CultureName), message));
 
             return this;
         }
 
         public async Task<Guard> AgainstTrueAsync<T>(
-            T? value, 
-            Func<T, Task<bool>> func, 
-            string? message = null, 
+            T? value,
+            Func<T, Task<bool>> func,
+            string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
             if (Continue && (value is null || await func.Invoke(value)))
-                Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+                Result = InvalidResult.Create(
+                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstTrue), paramName, CultureName), message));
 
             return this;
         }
 
         public Guard AgainstFalse(
-            bool? condition, 
-            string? message = null, 
+            bool? condition,
+            string? message = null,
             [CallerArgumentExpression(nameof(condition))] string? paramName = null)
         {
             if (Continue && condition is false or null)
-                Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+                Result = InvalidResult.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstFalse), paramName, CultureName), message));
 
             return this;
         }
 
         public Guard AgainstFalse<T>(
-            T? value, 
-            Func<T, bool> func, 
-            string? message = null, 
+            T? value,
+            Func<T, bool> func,
+            string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
             if (Continue && (value is null || !func.Invoke(value)))
-                Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
-
+                Result = InvalidResult.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstFalse), paramName, CultureName), message));
+            
             return this;
         }
 
         public async Task<Guard> AgainstFalseAsync<T>(
-            T? value, 
-            Func<T, Task<bool>> func, 
-            string? message = null, 
+            T? value,
+            Func<T, Task<bool>> func,
+            string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
             if (Continue && (value is null || !await func.Invoke(value)))
-                Result = InvalidResult.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+                Result = InvalidResult.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstFalse), paramName, CultureName), message));
 
             return this;
         }
@@ -80,71 +86,77 @@ namespace CleanValidation.Core.Guards
     public partial class Guard<T>
     {
         new public Guard<T> AgainstTrue(
-            bool? condition, 
-            string? message = null, 
+            bool? condition,
+            string? message = null,
             [CallerArgumentExpression(nameof(condition))] string? paramName = null)
         {
             if (Continue && condition is true or null)
-                Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstTrue), paramName, CultureName), message));
 
             return this;
         }
 
         new public Guard<T> AgainstTrue<TValue>(
-            TValue? value, 
-            Func<TValue, bool> func, 
-            string? message = null, 
+            TValue? value,
+            Func<TValue, bool> func,
+            string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
             if (Continue && (value is null || func.Invoke(value)))
-                Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstTrue), paramName, CultureName), message));
 
             return this;
         }
 
         new public async Task<Guard<T>> AgainstTrueAsync<TValue>(
-            TValue? value, 
-            Func<TValue, Task<bool>> func, 
-            string? message = null, 
+            TValue? value,
+            Func<TValue, Task<bool>> func,
+            string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
             if (Continue && (value is null || await func.Invoke(value)))
-                Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstTrue), paramName, CultureName), message));
 
             return this;
         }
 
         new public Guard<T> AgainstFalse(
-            bool? condition, 
-            string? message = null, 
+            bool? condition,
+            string? message = null,
             [CallerArgumentExpression(nameof(condition))] string? paramName = null)
         {
             if (Continue && condition is false or null)
-                Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstFalse), paramName, CultureName), message));
 
             return this;
         }
 
         new public Guard<T> AgainstFalse<TValue>(
-            TValue? value, 
-            Func<TValue, bool> func, 
-            string? message = null, 
+            TValue? value,
+            Func<TValue, bool> func,
+            string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
             if (Continue && (value is null || !func.Invoke(value)))
-                Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstFalse), paramName, CultureName), message));
 
             return this;
         }
 
         new public async Task<Guard<T>> AgainstFalseAsync<TValue>(
-            TValue? value, 
-            Func<TValue, Task<bool>> func, 
-            string? message = null, 
+            TValue? value,
+            Func<TValue, Task<bool>> func,
+            string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
             if (Continue && (value is null || !await func.Invoke(value)))
-                Result = InvalidResult<T>.Create(ErrorUtils.InvalidParameter(CultureName, paramName));
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstFalse), paramName, CultureName), message));
 
             return this;
         }
