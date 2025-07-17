11a3db52a261bb0e07aa068d163b9194885eceac
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-17 14:50:28 -0300

    ✨feat: Add Expression Extensions
    Adding extensions methods for Expression type

---
+++ src/CleanValidation.Core/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Linq.Expressions;
+
+namespace CleanValidation.Core.Extensions
+{
+    public static class ExpressionExtensions
+    {
+        public static string GetName<T, TProperty>(this Expression<Func<T, TProperty?>> propertyExpression)
+        {
+            if (propertyExpression is null || propertyExpression.Body is not MemberExpression memberExpression)
+                return string.Empty;
+
+            if (memberExpression.Member is not PropertyInfo propInfo)
+                return string.Empty;
+
+            if (!propInfo.ReflectedType!.IsAssignableFrom(typeof(T)))
+                return string.Empty;
+
+            return propInfo.Name;
+        }
+
+        public static TProperty? GetValue<T, TProperty>(
+            this Expression<Func<T, TProperty?>> propertyExpression,
+            T? value)
+        {
+            if (propertyExpression is null || value is null)
+                return default;
+
+            return propertyExpression.Compile().Invoke(value);
+        }
+    }
+}
