dd5eedf833ee8b8d6a556f452e572371e4c689d2
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-16 21:30:57 -0300

    ✨feat: Add StringExtensions
    Adding Format method to manipulate error messages easily

---
+++ src/CleanValidation.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanValidation.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Format(this string value, params object[]? args)
+        {
+            return string.Format(value, args ?? []);
+        }
+    }
+}
