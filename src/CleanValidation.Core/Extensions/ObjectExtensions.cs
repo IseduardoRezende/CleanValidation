e032290b693bb702b341c6c89a894b01f1c36710
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-17 18:32:28 -0300

    ✨feat: Add Object Extensions
    Adding extensions methods for Object type

---
+++ src/CleanValidation.Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace CleanValidation.Core.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static TType? To<TType>(this object? obj)
+        {
+            try
+            {
+                return (TType?)Convert.ChangeType(obj, typeof(TType?));
+            }
+            catch (Exception)
+            {
+                return default;
+            }
+        }
+    }
+}
