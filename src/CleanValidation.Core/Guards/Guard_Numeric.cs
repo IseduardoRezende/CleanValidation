cc76d31560caeb8002f4dc90990d3a562cdc27e4
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-21 16:32:12 -0300

    ✨feat: Add Guard_Numeric new methods
    Adding new methods for Guard_Numeric using Expression properties

--- src/CleanValidation.Core/Guards/Guard_Numeric.cs
+++ src/CleanValidation.Core/Guards/Guard_Numeric.cs
@@ -1,20 +1,25 @@
 ﻿using System.Numerics;
+using System.Linq.Expressions;
 using CleanValidation.Core.Errors;
 using CleanValidation.Core.Results;
 using System.Runtime.CompilerServices;
+using CleanValidation.Core.Extensions;
 
 namespace CleanValidation.Core.Guards
 {
     public partial class Guard
-    {        
+    {
         public Guard AgainstLessThan<T>(
-            T? value, 
-            T min, 
-            string? message = null, 
+            T? value,
+            T? min,
+            string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
                 where T : IComparisonOperators<T, T, bool>
         {
-            if (Continue && (value is null || value < min))
+            if (!Continue || value is null || min is null)
+                return this;
+
+            if (value < min)
                 Result = InvalidResult.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstLessThan), paramName, [min],
                     CultureName), message));
@@ -23,31 +28,37 @@ namespace CleanValidation.Core.Guards
         }
 
         public Guard AgainstGreaterThan<T>(
-            T? value, 
-            T max, 
-            string? message = null, 
+            T? value,
+            T? max,
+            string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
                 where T : IComparisonOperators<T, T, bool>
         {
-            if (Continue && (value is null || value > max))
+            if (!Continue || value is null || max is null)
+                return this;
+
+            if (value > max)
                 Result = InvalidResult.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstGreaterThan), paramName, [max],
                     CultureName), message));
 
             return this;
-        }                      
+        }
     }
 
     public partial class Guard<T>
-    {        
+    {
         new public Guard<T> AgainstLessThan<TValue>(
-            TValue? value, 
-            TValue min, 
-            string? message = null, 
+            TValue? value,
+            TValue? min,
+            string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
                 where TValue : IComparisonOperators<TValue, TValue, bool>
         {
-            if (Continue && (value is null || value < min))
+            if (!Continue || value is null || min is null)
+                return this;
+
+            if (value < min)
                 Result = InvalidResult<T>.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstLessThan), paramName, [min],
                     CultureName), message));
@@ -55,19 +66,66 @@ namespace CleanValidation.Core.Guards
             return this;
         }
 
+        public Guard<T> AgainstLessThan<TProperty>(
+            Expression<Func<T, TProperty?>> property,
+            TProperty? min,
+            string? message = null)
+                where TProperty : IComparisonOperators<TProperty, TProperty, bool>
+        {
+            if (!Continue || property is null || min is null)
+                return this;
+
+            TProperty? value = property.GetValue(Result.Value);
+
+            if (value is null)
+                return this;
+
+            if (value < min)
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstLessThan), property.GetName(), [min],
+                    CultureName), message));
+
+            return this;
+        }
+
         new public Guard<T> AgainstGreaterThan<TValue>(
-            TValue? value, 
-            TValue max, 
-            string? message = null, 
+            TValue? value,
+            TValue? max,
+            string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
                 where TValue : IComparisonOperators<TValue, TValue, bool>
         {
-            if (Continue && (value is null || value > max))
+            if (!Continue || value is null || max is null)
+                return this;
+
+            if (value > max)
                 Result = InvalidResult<T>.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstGreaterThan), paramName, [max],
                     CultureName), message));
 
             return this;
-        }                      
+        }
+
+        public Guard<T> AgainstGreaterThan<TProperty>(
+            Expression<Func<T, TProperty?>> property,
+            TProperty? max,
+            string? message = null)
+                where TProperty : IComparisonOperators<TProperty, TProperty, bool>
+        {
+            if (!Continue || property is null || max is null)
+                return this;
+
+            TProperty? value = property.GetValue(Result.Value);
+
+            if (value is null)
+                return this;
+
+            if (value > max)
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstGreaterThan), property.GetName(), [max],
+                    CultureName), message));
+
+            return this;
+        }
     }
 }
