317ce14dfa6f6703a1c6416f383e41c9deaeb888
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-21 17:35:44 -0300

    ✨feat: Add Guard_Type new methods
    Adding new methods for Guard_Type using Expression properties

--- src/CleanValidation.Core/Guards/Guard_Type.cs
+++ src/CleanValidation.Core/Guards/Guard_Type.cs
@@ -1,5 +1,7 @@
-﻿using CleanValidation.Core.Errors;
+﻿using System.Linq.Expressions;
+using CleanValidation.Core.Errors;
 using CleanValidation.Core.Results;
+using CleanValidation.Core.Extensions;
 using System.Runtime.CompilerServices;
 
 namespace CleanValidation.Core.Guards
@@ -11,7 +13,10 @@ namespace CleanValidation.Core.Guards
             string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
-            if (Continue && (value is null || !value.GetType().IsAssignableTo(typeof(TBase))))
+            if (!Continue || value is null)
+                return this;
+
+            if (!value.GetType().IsAssignableTo(typeof(TBase)))
                 Result = InvalidResult.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstNotAssignableTo), paramName, [typeof(TBase).Name],
                     CultureName), message));
@@ -24,7 +29,10 @@ namespace CleanValidation.Core.Guards
             string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
-            if (Continue && (value is null || !value.GetType().IsAssignableFrom(typeof(TBase))))
+            if (!Continue || value is null)
+                return this;
+
+            if (!value.GetType().IsAssignableFrom(typeof(TBase)))
                 Result = InvalidResult.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstNotAssignableFrom), paramName, [typeof(TBase).Name],
                     CultureName), message));
@@ -37,7 +45,10 @@ namespace CleanValidation.Core.Guards
             string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
-            if (Continue && (value is null || !value.GetType().Equals(typeof(TExpected))))
+            if (!Continue || value is null)
+                return this;
+
+            if (!value.GetType().Equals(typeof(TExpected)))
                 Result = InvalidResult.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstNotTypeOf), paramName, [typeof(TExpected).Name],
                     CultureName), message));
@@ -50,7 +61,10 @@ namespace CleanValidation.Core.Guards
             string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
-            if (Continue && (value is null || value.GetType().IsAssignableTo(typeof(TBase))))
+            if (!Continue || value is null)
+                return this;
+
+            if (value.GetType().IsAssignableTo(typeof(TBase)))
                 Result = InvalidResult.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstAssignableTo), paramName, [typeof(TBase).Name],
                     CultureName), message));
@@ -63,7 +77,10 @@ namespace CleanValidation.Core.Guards
             string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
-            if (Continue && (value is null || value.GetType().IsAssignableFrom(typeof(TBase))))
+            if (!Continue || value is null)
+                return this;
+
+            if (value.GetType().IsAssignableFrom(typeof(TBase)))
                 Result = InvalidResult.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstAssignableFrom), paramName, [typeof(TBase).Name],
                     CultureName), message));
@@ -76,7 +93,10 @@ namespace CleanValidation.Core.Guards
             string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
-            if (Continue && (value is null || value.GetType().Equals(typeof(TExpected))))
+            if (!Continue || value is null)
+                return this;
+
+            if (value.GetType().Equals(typeof(TExpected)))
                 Result = InvalidResult.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstTypeOf), paramName, [typeof(TExpected).Name],
                     CultureName), message));
@@ -90,7 +110,10 @@ namespace CleanValidation.Core.Guards
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
                 where TEnum : Enum
         {
-            if (Continue && (value is null || !Enum.IsDefined(typeof(TEnum), value)))
+            if (!Continue || value is null)
+                return this;
+
+            if (!Enum.IsDefined(typeof(TEnum), value))
                 Result = InvalidResult.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstInvalidEnum), paramName, cultureName: CultureName), message));
 
@@ -105,9 +128,32 @@ namespace CleanValidation.Core.Guards
             string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
-            if (Continue && (value is null || !value.GetType().IsAssignableTo(typeof(TBase))))
+            if (!Continue || value is null)
+                return this;
+
+            if (!value.GetType().IsAssignableTo(typeof(TBase)))
                 Result = InvalidResult<T>.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstNotAssignableTo), paramName, [typeof(TBase).Name],
+                    CultureName), message));
+
+            return this;
+        }
+
+        public Guard<T> AgainstNotAssignableTo<TBase>(
+            Expression<Func<T, object?>> property,
+            string? message = null)
+        {
+            if (!Continue || property is null)
+                return this;
+
+            object? value = property.GetValue(Result.Value);
+
+            if (value is null)
+                return this;
+
+            if (!value.GetType().IsAssignableTo(typeof(TBase)))
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstNotAssignableTo), property.GetName(), [typeof(TBase).Name],
                     CultureName), message));
 
             return this;
@@ -118,9 +164,32 @@ namespace CleanValidation.Core.Guards
             string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
-            if (Continue && (value is null || !value.GetType().IsAssignableFrom(typeof(TBase))))
+            if (!Continue || value is null)
+                return this;
+
+            if (!value.GetType().IsAssignableFrom(typeof(TBase)))
                 Result = InvalidResult<T>.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstNotAssignableFrom), paramName, [typeof(TBase).Name],
+                    CultureName), message));
+
+            return this;
+        }
+
+        public Guard<T> AgainstNotAssignableFrom<TBase>(
+            Expression<Func<T, object?>> property,
+            string? message = null)
+        {
+            if (!Continue || property is null)
+                return this;
+
+            object? value = property.GetValue(Result.Value);
+
+            if (value is null)
+                return this;
+
+            if (!value.GetType().IsAssignableFrom(typeof(TBase)))
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstNotAssignableFrom), property.GetName(), [typeof(TBase).Name],
                     CultureName), message));
 
             return this;
@@ -131,9 +200,32 @@ namespace CleanValidation.Core.Guards
             string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
-            if (Continue && (value is null || !value.GetType().Equals(typeof(TExpected))))
+            if (!Continue || value is null)
+                return this;
+
+            if (!value.GetType().Equals(typeof(TExpected)))
                 Result = InvalidResult<T>.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstNotTypeOf), paramName, [typeof(TExpected).Name],
+                    CultureName), message));
+
+            return this;
+        }
+
+        public Guard<T> AgainstNotTypeOf<TExpected>(
+            Expression<Func<T, object?>> property,
+            string? message = null)
+        {
+            if (!Continue || property is null)
+                return this;
+
+            object? value = property.GetValue(Result.Value);
+
+            if (value is null)
+                return this;
+
+            if (!value.GetType().Equals(typeof(TExpected)))
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstNotTypeOf), property.GetName(), [typeof(TExpected).Name],
                     CultureName), message));
 
             return this;
@@ -144,9 +236,32 @@ namespace CleanValidation.Core.Guards
             string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
-            if (Continue && (value is null || value.GetType().IsAssignableTo(typeof(TBase))))
+            if (!Continue || value is null)
+                return this;
+
+            if (value.GetType().IsAssignableTo(typeof(TBase)))
                 Result = InvalidResult<T>.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstAssignableTo), paramName, [typeof(TBase).Name],
+                    CultureName), message));
+
+            return this;
+        }
+
+        public Guard<T> AgainstAssignableTo<TBase>(
+            Expression<Func<T, object?>> property,
+            string? message = null)
+        {
+            if (!Continue || property is null)
+                return this;
+
+            object? value = property.GetValue(Result.Value);
+
+            if (value is null)
+                return this;
+
+            if (value.GetType().IsAssignableTo(typeof(TBase)))
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstAssignableTo), property.GetName(), [typeof(TBase).Name],
                     CultureName), message));
 
             return this;
@@ -157,9 +272,32 @@ namespace CleanValidation.Core.Guards
             string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
-            if (Continue && (value is null || value.GetType().IsAssignableFrom(typeof(TBase))))
+            if (!Continue || value is null)
+                return this;
+
+            if (value.GetType().IsAssignableFrom(typeof(TBase)))
                 Result = InvalidResult<T>.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstAssignableFrom), paramName, [typeof(TBase).Name],
+                    CultureName), message));
+
+            return this;
+        }
+
+        public Guard<T> AgainstAssignableFrom<TBase>(
+            Expression<Func<T, object?>> property,
+            string? message = null)
+        {
+            if (!Continue || property is null)
+                return this;
+
+            object? value = property.GetValue(Result.Value);
+
+            if (value is null)
+                return this;
+
+            if (value.GetType().IsAssignableFrom(typeof(TBase)))
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstAssignableFrom), property.GetName(), [typeof(TBase).Name],
                     CultureName), message));
 
             return this;
@@ -170,23 +308,69 @@ namespace CleanValidation.Core.Guards
             string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
-            if (Continue && (value is null || value.GetType().Equals(typeof(TExpected))))
+            if (!Continue || value is null)
+                return this;
+
+            if (value.GetType().Equals(typeof(TExpected)))
                 Result = InvalidResult<T>.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstTypeOf), paramName, [typeof(TExpected).Name],
                     CultureName), message));
 
             return this;
         }
-        
+
+        public Guard<T> AgainstTypeOf<TExpected>(
+            Expression<Func<T, object?>> property,
+            string? message = null)
+        {
+            if (!Continue || property is null)
+                return this;
+
+            object? value = property.GetValue(Result.Value);
+
+            if (value is null)
+                return this;
+
+            if (value.GetType().Equals(typeof(TExpected)))
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstTypeOf), property.GetName(), [typeof(TExpected).Name],
+                    CultureName), message));
+
+            return this;
+        }
+
         new public Guard<T> AgainstInvalidEnum<TEnum>(
             object? value,
             string? message = null,
             [CallerArgumentExpression(nameof(value))] string? paramName = null)
                 where TEnum : Enum
         {
-            if (Continue && (value is null || !Enum.IsDefined(typeof(TEnum), value)))
+            if (!Continue || value is null)
+                return this;
+
+            if (!Enum.IsDefined(typeof(TEnum), value))
                 Result = InvalidResult<T>.Create(
                     ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstInvalidEnum), paramName, cultureName: CultureName), message));
+
+            return this;
+        }
+
+        public Guard<T> AgainstInvalidEnum<TEnum>(
+            Expression<Func<T, object?>> property,
+            string? message = null)
+                where TEnum : Enum
+        {
+            if (!Continue || property is null)
+                return this;
+
+            object? value = property.GetValue(Result.Value);
+
+            if (value is null)
+                return this;
+
+            if (!Enum.IsDefined(typeof(TEnum), value))
+                Result = InvalidResult<T>.Create(
+                    ErrorUtils.Custom(ErrorUtils.GetByKey(nameof(AgainstInvalidEnum), property.GetName(), cultureName: CultureName), message));
 
             return this;
         }
