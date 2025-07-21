a6ca906948b531883a05ee25f7a462213c6548f4
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-21 20:13:51 -0300

    ✨feat: Add Guard_File new methods
    Adding new methods for Guard_File using Expression properties and concrete data

--- src/CleanValidation.Core/Guards/Guard_File.cs
+++ src/CleanValidation.Core/Guards/Guard_File.cs
@@ -1,37 +1,109 @@
-﻿using CleanValidation.Core.Results;
+﻿using System.Linq.Expressions;
+using CleanValidation.Core.Options;
+using CleanValidation.Core.Results;
+using CleanValidation.Core.Extensions;
 using System.Runtime.CompilerServices;
 
 namespace CleanValidation.Core.Guards
 {
     public partial class Guard
-    {        
+    {
         public Guard AgainstInvalidContentTypes(
-            IEnumerable<byte>? bytes, 
-            IEnumerable<string> types,
-            string? message = null, 
-            [CallerArgumentExpression(nameof(bytes))] string? paramName = null)
+            IEnumerable<byte>? fileData,
+            IEnumerable<ContentType>? contentTypes,
+            string? message = null,
+            [CallerArgumentExpression(nameof(fileData))] string? paramName = null)
         {
-            if (Continue && !IsValidContentType(bytes, types))
+            if (!Continue || fileData is null || contentTypes is null)
+                return this;
+
+            if (ContentTypeOptions.ContainsType(fileData, contentTypes))
                 Result = InvalidResult.Create([]);
-                
+
             return this;
         }
 
-        protected static bool IsValidContentType(IEnumerable<byte>? bytes, IEnumerable<string> types)
+        public Guard AgainstNotExactContentType(
+            IEnumerable<byte>? fileData,
+            ContentType contentType,
+            string? message = null,
+            [CallerArgumentExpression(nameof(fileData))] string? paramName = null)
         {
-            return false;
+            if (!Continue || fileData is null)
+                return this;
+
+            if (!ContentTypeOptions.ContainsType(fileData, contentType))
+                Result = InvalidResult.Create([]);
+
+            return this;
         }
     }
 
     public partial class Guard<T>
-    {       
+    {
         new public Guard<T> AgainstInvalidContentTypes(
-            IEnumerable<byte>? bytes, 
-            IEnumerable<string> types, 
-            string? message = null, 
-            [CallerArgumentExpression(nameof(bytes))] string? paramName = null)
+            IEnumerable<byte>? fileData,
+            IEnumerable<ContentType>? contentTypes,
+            string? message = null,
+            [CallerArgumentExpression(nameof(fileData))] string? paramName = null)
         {
-            if (Continue && !IsValidContentType(bytes, types))
+            if (!Continue || fileData is null || contentTypes is null)
+                return this;
+
+            if (ContentTypeOptions.ContainsType(fileData, contentTypes))
+                Result = InvalidResult<T>.Create([]);
+
+            return this;
+        }
+
+        public Guard<T> AgainstInvalidContentTypes(
+            Expression<Func<T, IEnumerable<byte>?>> property,
+            IEnumerable<ContentType>? contentTypes,
+            string? message = null)
+        {
+            if (!Continue || property is null || contentTypes is null)
+                return this;
+
+            IEnumerable<byte>? fileData = property.GetValue(Result.Value);
+
+            if (fileData is null)
+                return this;
+
+            if (ContentTypeOptions.ContainsType(fileData, contentTypes))
+                Result = InvalidResult<T>.Create([]);
+
+            return this;
+        }
+
+        new public Guard<T> AgainstNotExactContentType(
+            IEnumerable<byte>? fileData,
+            ContentType contentType,
+            string? message = null,
+            [CallerArgumentExpression(nameof(fileData))] string? paramName = null)
+        {
+            if (!Continue || fileData is null)
+                return this;
+
+            if (!ContentTypeOptions.ContainsType(fileData, contentType))
+                Result = InvalidResult<T>.Create([]);
+
+            return this;
+        }
+
+        public Guard<T> AgainstNotExactContentType(
+            Expression<Func<T, IEnumerable<byte>?>> property,
+             ContentType contentType,
+            string? message = null)
+        {
+            if (!Continue || property is null)
+                return this;
+
+            IEnumerable<byte>? fileData = property.GetValue(Result.Value);
+
+            if (fileData is null)
+                return this;
+
+            if (!ContentTypeOptions.ContainsType(fileData, contentType))
                 Result = InvalidResult<T>.Create([]);
 
             return this;
