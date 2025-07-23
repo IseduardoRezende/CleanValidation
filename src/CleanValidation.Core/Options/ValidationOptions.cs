e61b0bf668f96de8bbe0a3d8964336551f34a87e
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-23 13:48:35 -0300

    ✨feat: Add ValidationOptions
    Adding enum to represent Guard validation behavior

---
+++ src/CleanValidation.Core/Options/ValidationOptions.cs
@@ -0,0 +1,29 @@
+﻿namespace CleanValidation.Core.Options
+{
+    /// <summary>
+    /// The validation options available to configure the behavior of 
+    /// <see cref="Guards.Guard"/> validations.
+    /// </summary>
+    public enum ValidationOptions
+    {
+        /// <summary>
+        /// Represents whether the <see cref="Guards.Guard"/> validations should continue execution 
+        /// even if an <see cref="Errors.Error"/> occurs.
+        /// </summary>
+        /// <remarks>
+        /// The operation will proceed despite
+        /// encountering errors. This can be useful to accumulate errors. 
+        /// </remarks>
+        ContinueOnFailure,
+
+        /// <summary>
+        /// Represents whether the <see cref="Guards.Guard"/> validations should stop execution 
+        /// when an <see cref="Errors.Error"/> occurs.
+        /// </summary>
+        /// <remarks>
+        /// The operation will stop when encountering an error. 
+        /// This can be useful to not accumulate errors. 
+        /// </remarks>
+        FailFirst
+    }
+}
