5e19aa8ea85c8a8ff818ada2222da94dfddba5f9
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-23 16:30:55 -0300

    ✨feat: Add ErrorBag
    Adding ErrorBag class to encapsulate Guard errors

---
+++ src/CleanValidation.Core/Errors/ErrorBag.cs
@@ -0,0 +1,21 @@
+﻿using CleanValidation.Core.Exceptions;
+
+namespace CleanValidation.Core.Errors
+{
+    public class ErrorBag
+    {
+        private readonly List<Error> _errors = [];
+
+        public void Add(Error error)
+        {
+            if (error is null)
+                throw new CleanValidationException($"{nameof(error)} can't be null.");
+
+            _errors.Add(error);
+        }
+
+        public int Count { get { return _errors.Count; } }
+
+        public List<Error> Errors { get { return _errors; } }
+    }
+}
