0a91a1960bb2ead479f2e14355daf63c4ff401c5
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-30 07:52:34 -0300

    ✨feat: Add CleanValidationException new method
    Adding ThrowIfNull method

--- src/CleanValidation.Core/Exceptions/CleanValidationException.cs
+++ src/CleanValidation.Core/Exceptions/CleanValidationException.cs
@@ -1,10 +1,12 @@
-﻿namespace CleanValidation.Core.Exceptions
+﻿using System.Runtime.CompilerServices;
+
+namespace CleanValidation.Core.Exceptions
 {
     /// <summary>
     /// Represents clean validation errors that occur during application execution. 
     /// </summary>
     public class CleanValidationException : Exception
-    {        
+    {
         /// <summary>
         /// Initializes a new instance of the <see cref="CleanValidationException"/> class.
         /// </summary>
@@ -16,7 +18,15 @@
         /// </summary>
         /// <param name="message">Descriptive message.</param>
         /// <param name="innerException">The inner exception.</param>
-        public CleanValidationException(string message, Exception? innerException) 
+        public CleanValidationException(string message, Exception? innerException)
             : base(message, innerException) { }
+
+        public static void ThrowIfNull(
+            object? obj,
+            [CallerArgumentExpression(nameof(obj))] string? paramName = null)
+        {
+            if (obj is null)
+                throw new CleanValidationException($"{paramName} can't be null.");
+        }
     }
 }
