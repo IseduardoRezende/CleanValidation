36ee1aa5a396d6e2a2b4c72ed30e520d4cc01de9
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-29 22:45:17 -0300

    ✨feat: Add Phone validator
    Adding class to validate phone values

---
+++ src/CleanValidation.Core/Validators/Core/PhoneValidator.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanValidation.Core.Validators.Core
+{
+    internal class PhoneValidator
+    {
+        public static bool IsValid(string? phone)
+        {
+            return phone is not null && new PhoneAttribute().IsValid(phone);
+        }
+    }
+}
