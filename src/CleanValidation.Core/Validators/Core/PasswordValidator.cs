8c8be009636a76a17dc470989424fc4f8bbb0875
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-29 22:45:02 -0300

    ✨feat: Add Password validator
    Adding class to validate password values

---
+++ src/CleanValidation.Core/Validators/Core/PasswordValidator.cs
@@ -0,0 +1,36 @@
+﻿using CleanValidation.Core.Options;
+
+namespace CleanValidation.Core.Validators.Core
+{
+    internal class PasswordValidator
+    {
+        public static bool IsValid(
+            string? password,
+            PasswordOptions? options)
+        {
+            if (string.IsNullOrWhiteSpace(password) || options is null)
+                return false;
+
+            if (password.Length < options.MinLength || password.Length > options.MaxLength)
+                return false;
+
+            if (options.RequireDigit && !password.Any(char.IsDigit))
+                return false;
+
+            if (options.RequireUpper && !password.Any(char.IsUpper))
+                return false;
+
+            if (options.RequireLower && !password.Any(char.IsLower))
+                return false;
+
+            if (options.RequireSpecial && !password.Any(c => !char.IsLetterOrDigit(c)))
+                return false;
+
+            if (options.DisallowSequences && StringValidator.ContainsSequence(password))
+                return false;
+
+            return true;
+        }
+
+    }
+}
