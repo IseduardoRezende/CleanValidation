658b91ea7b380e83f9bc9452ce1ae10370504950
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-29 22:43:46 -0300

    ✨feat: Add EmailAddress validator
    Adding class to validate email values

---
+++ src/CleanValidation.Core/Validators/Core/EmailAddressValidator.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanValidation.Core.Validators.Core
+{
+    internal class EmailAddressValidator
+    {
+        public const int MaxEmailAddressLength = 320;
+
+        public static bool IsValid(string? email)
+        {
+            return email is { Length: <= MaxEmailAddressLength } &&
+                   new EmailAddressAttribute().IsValid(email);
+        }
+    }
+}
