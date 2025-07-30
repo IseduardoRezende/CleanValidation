b1fd8ae4796e2d7fbf082264571fe9686978ea60
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-29 22:46:09 -0300

    ✨feat: Add Uri validator
    Adding class to validate uri values

---
+++ src/CleanValidation.Core/Validators/Core/UriValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace CleanValidation.Core.Validators.Core
+{
+    internal class UriValidator
+    {
+        public static bool IsValid(
+           string? uri,
+           UriKind kind = UriKind.Absolute)
+        {
+            if (!Uri.IsWellFormedUriString(uri, kind) || !Uri.TryCreate(uri, kind, out Uri? outUri))
+                return false;
+
+            return outUri.Scheme == Uri.UriSchemeHttp || outUri.Scheme == Uri.UriSchemeHttps;
+        }
+    }
+}
