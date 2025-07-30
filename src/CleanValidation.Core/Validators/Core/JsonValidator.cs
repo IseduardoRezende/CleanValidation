68204bfa27d1229f9767aadbe931cfc492efce57
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-29 22:44:40 -0300

    ✨feat: Add Json validator
    Adding class to validate json values

---
+++ src/CleanValidation.Core/Validators/Core/JsonValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+namespace CleanValidation.Core.Validators.Core
+{
+    internal class JsonValidator
+    {
+        public static bool IsValid<T>(string? json, JsonSerializerOptions? options = null)
+        {
+            if (string.IsNullOrWhiteSpace(json))
+                return false;
+
+            try
+            {
+                _ = JsonSerializer.Deserialize<T>(json, options);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
