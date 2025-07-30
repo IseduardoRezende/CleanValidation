70a8c54b4e0f8d30adb8e5a00d0db6715499731e
Eduardo Rezende <dudumoises2005@gmail.com>
2025-07-29 22:45:48 -0300

    ✨feat: Add String validator
    Adding class to validate string values

---
+++ src/CleanValidation.Core/Validators/Core/StringValidator.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+
+namespace CleanValidation.Core.Validators.Core
+{
+    internal class StringValidator
+    {
+        public static bool ContainsHtml(string? value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return false;
+
+            string html = WebUtility.HtmlEncode(value);
+            return !html.Equals(value);
+        }
+
+        public static bool ContainsSequence(string? value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return false;
+
+            for (int i = 0; i < value.Length - 1; i++)
+            {
+                char current = value[i];
+                char next = value[i + 1];
+
+                // Only Chars or Digits
+                if ((char.IsLower(current) && char.IsLower(next)) ||
+                    (char.IsUpper(current) && char.IsUpper(next)) ||
+                    (char.IsDigit(current) && char.IsDigit(next)))
+                {
+                    // Using table ASCII
+                    if (next - current == 1)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
