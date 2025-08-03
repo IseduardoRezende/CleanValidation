1919fa9c7d718007389e531631000bce5f7a6dcb
Eduardo Rezende <dudumoises2005@gmail.com>
2025-08-03 13:49:12 -0300

    ✨feat: Add GuardThrowExtensions
    Adding extension method for GuardThrow

---
+++ src/CleanValidation.Core/Extensions/GuardThrowExtensions.cs
@@ -0,0 +1,12 @@
+﻿using CleanValidation.Core.GuardThrows;
+
+namespace CleanValidation.Core.Extensions
+{
+    public static class GuardThrowExtensions
+    {
+        public static GuardThrow<T> GuardThrow<T>(this T? value, string cultureName = "en-US")
+        {
+            return GuardThrows.GuardThrow<T>.Create(value, cultureName);
+        }
+    }
+}
